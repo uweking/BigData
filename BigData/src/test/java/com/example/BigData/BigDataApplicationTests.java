27c712bb7e55c69eca1e23b3452da24e71c0020e
uweking <klausuwekoenig@gmail.com>
2021-06-08 14:30:48 +0200

    init

---
+++ BigData/src/test/java/com/example/BigData/BigDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BigData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
